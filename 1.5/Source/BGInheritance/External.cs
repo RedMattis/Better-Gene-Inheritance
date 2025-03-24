96868db8c66aba86f69717be0755c3946bf2d5ff
RedMattis <redmattis@gmail.com>
2025-03-24 14:38:14 +0100

    Fixed accidentally removed public method.

---
+++ 1.5/Source/BGInheritance/External.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BGInheritance
+{
+    /// <summary>
+    /// Methods made to be called by other mods.
+    /// </summary>
+    public static class External
+    {
+        public static List<GeneDef> GetChildGenes(Pawn parentA, Pawn parentB)
+        {
+            var geneDefs = new List<GeneDef>();
+            bool _ = false;
+            HarmonyPatches.InheritancePrefix(ref geneDefs, parentA, parentB, ref _);
+            return geneDefs;
+        }
+    }
+}
